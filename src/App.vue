306e658730db044b1c1a9241cd5c9d88968e0d34
dzrzzzkal <1844192925@qq.com>
2022-07-06 23:52:21 +0800

    feat:增加tabbar中

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Tabbar from '@/components/tabbar/Tabbar.vue'
 </script>
 
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" /> -->
   <router-view></router-view>
+  <tabbar>
+    <div>首页</div>
+  </tabbar>
 </template>
 
 <style>
---
+++ src/components/tabbar/Tabbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tabbar-line">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+.tabbar-line {
+  width: 100%;
+  height: 56px;
+  position: fixed;
+  bottom: 0;
+}
+.tabbar-line::before {
+  content: '';
+  height: 1px;
+  display: block;
+  transform: scaleY(0.5);
+  background: gray;
+}
+</style>
